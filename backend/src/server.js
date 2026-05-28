fa6c06392f50554f3c82e9608d9d44c10a170bbb
chandrakant2005 <rautchandrakant430@gmail.com>
2026-05-28 17:12:57 +0530

    auth db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,68 @@
+import cors from "cors";
 import express from "express";
+import { serve } from "inngest/express";
+
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import { functions, inngest } from "./lib/inngest.js";
+
 const app = express();
 
-console.log();
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.get("/",(req,res) => {
-    res.status(200).json({msg:"success from backend"})
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "success from backend" });
 });
 
-// app.listen(ENV.PORT,() => {
-//     console.log("server is running on port:",ENV.PORT);
-//     connectDB();
-// });
-
 const startServer = async () => {
-    try{
-        await connectDB();
-        app.listen(ENV.PORT, () => console.log("Server is running on port:",ENV.PORT));
-    }catch(error){
-        console.error("Error starting the server",error);
-    }
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port:", ENV.PORT)
+    );
+  } catch (error) {
+    console.error("Error starting the server", error);
+  }
 };
 
 startServer();
+
+// import express from "express";
+
+// import cors from "cors";
+// import { serve } from "inngest/express";
+
+
+// import { connectDB } from "./lib/db.js";
+// import { ENV } from "./lib/env.js";
+// const app = express();
+
+
+// // middleware
+// app.use(express.json());
+// app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+// app.use("/api/inngest", serve({client:inngest,functions}));
+
+// app.get("/",(req,res) => {
+//     res.status(200).json({msg:"success from backend"})
+// });
+
+// // app.listen(ENV.PORT,() => {
+// //     console.log("server is running on port:",ENV.PORT);
+// //     connectDB();
+// // });
+
+// const startServer = async () => {
+//     try{
+//         await connectDB();
+//         app.listen(ENV.PORT, () => console.log("Server is running on port:",ENV.PORT));
+//     }catch(error){
+//         console.error("Error starting the server",error);
+//     }
+// };
+
+// startServer();
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,30 @@
-import { SignInButton } from "@clerk/clerk-react";
-import './App.css';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton
+} from "@clerk/clerk-react";
+
+import "./App.css";
+
 function App() {
   return (
     <>
-    <h1>Welcome to the page!</h1>
-    <SignedOut>
-      <SignInButton mode="modal">
-        <button className="">Login</button>
-      </SignInButton>
-    </SignedOut>
+      <h1>Welcome to the page!</h1>
 
-    <SignedIn>
-      <SignOutButton/>
-    </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button>Login</button>
+        </SignInButton>
+      </SignedOut>
 
-    <UserButton/>
+      <SignedIn>
+        <SignOutButton />
+        <UserButton />
+      </SignedIn>
     </>
   );
 }
 
-export default App
+export default App;
