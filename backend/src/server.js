7bfcf8f496d181a57990c48f8ddeb6334a89c85b
chandrakant2005 <rautchandrakant430@gmail.com>
2026-06-01 11:41:27 +0530

    server side auth implementd

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,28 @@
+import { clerkMiddleware } from '@clerk/express';
 import cors from "cors";
 import express from "express";
 import { serve } from "inngest/express";
+
 
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { functions, inngest } from "./lib/inngest.js";
 
+import chatRoutes from "./routes/chatRoutes.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat",chatRoutes);
+
+app.get("/health",(req,res) => {
+  req.auth;
+  res.status(200).json({msg: "api is up and running" });
+});
+
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "success from backend" });
