75eeb03024484441bbebcb0f14ed4544a880ab35
chandrakant2005 <rautchandrakant430@gmail.com>
2026-06-06 16:34:11 +0530

    frontend tools setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,18 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignOutButton,
-  UserButton
-} from "@clerk/clerk-react";
-
-import "./App.css";
+import { useUser } from "@clerk/clerk-react";
+import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import ProblemsPage from "./pages/ProblemsPage";
 
 function App() {
+  const {isSignedIn}=useUser();
   return (
     <>
-      <h1>Welcome to the page!</h1>
-
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button>Login</button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <SignOutButton />
-        <UserButton />
-      </SignedIn>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/problems" element={isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}/>
+    </Routes>
+    <Toaster position="top-center" toastOptions={{duration:3000}}/>
     </>
   );
 }
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+function HomePage(){
+    return (
+        <div>
+            <button className="btn btn-secondary" onClick={() => toast.success("This is a successtoast")}>Click me</button>
+
+            <SignedOut>
+                <SignInButton mode="modal">
+                    <button>Login</button>
+                </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+                <SignOutButton/>
+            </SignedIn>
+
+            <UserButton/>
+        </div>
+    );
+}
+
+export default HomePage;
